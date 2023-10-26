c54c46d34fcf6059830fae7909d5caf001064443
Manuel Bertoluzza <manuel.bertoluzza@zupit.it>
2023-10-26 22:57:40 +0200

    feat: tests

---
+++ zupit-standards-dotnet-IDE0072-tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace zupit_standards_dotnet_IDE0072_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
