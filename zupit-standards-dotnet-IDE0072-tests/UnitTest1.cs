17f5542df1c625043c60055bd27aece81bc35ba1
Manuel Bertoluzza <manuel.bertoluzza@zupit.it>
2023-10-26 23:09:39 +0200

    feat: sample switch with tests

--- zupit-standards-dotnet-IDE0072-tests/UnitTest1.cs
+++ zupit-standards-dotnet-IDE0072-tests/UnitTest1.cs
@@ -1,15 +1,37 @@
+using zupit_standards_dotnet_IDE0072;
+
 namespace zupit_standards_dotnet_IDE0072_tests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
 
     [Test]
-    public void Test1()
+    public void TestGetValueEnumItem1()
     {
-        Assert.Pass();
+        Assert.That(MyProgram.GetValue(MyEnum.Item1), Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void TestGetValueEnumItem2()
+    {
+        Assert.That(MyProgram.GetValue(MyEnum.Item2), Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void TestGetValueEnumItem3()
+    {
+        Assert.That(MyProgram.GetValue(MyEnum.Item3), Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void TestGetValueEnumItem4()
+    {
+        Assert.That(MyProgram.GetValue(MyEnum.Item4), Is.EqualTo(2));
+    }
+    
+    [Test]
+    public void TestGetValueEnumItem5()
+    {
+        Assert.That(MyProgram.GetValue(MyEnum.Item5), Is.EqualTo(0));
     }
 }
--- zupit-standards-dotnet-IDE0072/Program.cs
+++ zupit-standards-dotnet-IDE0072/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace zupit_standards_dotnet_IDE0072;
 
-Console.WriteLine("Hello, World!");
+// An enum with 20 items
+public enum MyEnum
+{
+    Item1,
+    Item2,
+    Item3,
+    Item4,
+    Item5,
+    Item6,
+    Item7,
+    Item8,
+    Item9,
+    Item10,
+    Item11,
+    Item12,
+    Item13,
+    Item14,
+    Item15,
+    Item16,
+    Item17,
+    Item18,
+    Item19,
+    Item20
+};
+
+public class MyProgram
+{
+    public static int GetValue(MyEnum randomItem)
+    {
+        // A switch expression with a few cases different than the default case
+        var value = randomItem switch
+        {
+            MyEnum.Item1 or MyEnum.Item2 or MyEnum.Item3 => 1,
+            MyEnum.Item4 => 2,
+            _ => 0
+        };
+        return value;
+    }
+
+    static void Main()
+    {
+    }
+}
